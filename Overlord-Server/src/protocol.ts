76e0bc77c0ed8deae1947856fb74c79cb53c80a3
KingKDot <why_are_you_looking_at_this@stoppeeping.com>
2026-07-31 01:47:07 -0500

    add a little more plugin options for people who make plugins that need to be auto loaded.

--- Overlord-Server/src/protocol.ts
+++ Overlord-Server/src/protocol.ts
@@ -345,6 +345,7 @@ export type PluginManifest = {
   apiVersion?: number;
   runtime?: "native" | "wasm" | "server" | string;
   nativeLoader?: "memory" | "os" | string;
+  autoLoadByDefault?: boolean;
   nativeEntrypoints?: PluginNativeEntrypoints;
   version?: string;
   description?: string;
